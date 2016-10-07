bbc791ab5152f0c314e1a0b8f1ad6f1d61d0871a
lenovo <lenovo@idea-PC>
2016-10-07 21:29:33 +0800

    1

--- src/Solution2.java
+++ src/Solution2.java
@@ -1,4 +1,30 @@
+import java.util.ArrayList;
+//import java.util.Vector;
+
 
 public class Solution2 {
-
+    public void reOrderArray(int [] array) {
+       //Vector v=new Vector();
+    	
+        ArrayList<Integer> v=new ArrayList<Integer>();
+        for(int i=0;i<array.length;i++)
+            {
+            if(array[i]%2==1)
+                {
+                v.add(array[i]);
+            }
+             
+        }
+        for(int j=0;j<array.length;j++)
+            {
+            if(array[j]%2==0)
+                {
+                v.add(array[j]);
+            }
+        }
+        for(int i=0;i<array.length;i++)
+        {
+         array[i]=(int)v.get(i);
+        }
+    }
 }
